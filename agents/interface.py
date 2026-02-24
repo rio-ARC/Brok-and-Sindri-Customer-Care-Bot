31f05bdb47b79e062dc8bddd53e6e3f180c04fb1
Aritra Roy Choudhury <arc.nbg11@gmail.com>
2026-02-24 09:52:38 +0530

    Update interface routing logic

--- agents/interface.py
+++ agents/interface.py
@@ -5,17 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 router_llm = ChatGroq(
-    temperature=0,  # <--- CRITICAL: Keeps it strict
+    temperature=0, 
     model="llama-3.3-70b-versatile",
     api_key=os.getenv("GROQ_API_KEY"),
-    max_tokens=10   # It only needs to say one word
+    max_tokens=10
 )
 
 
 
 def interface_llm(state):
-    # We use a specific "Router Prompt" here. 
-    # It doesn't roleplay yet; it just sorts the mail.
+    
     prompt = ChatPromptTemplate.from_messages([
         ("system", """
         You are the Gatekeeper for Huldra Brothers Workshop.
@@ -34,23 +33,19 @@ def interface_llm(state):
     
     chain = prompt | router_llm
     
-    # Get the last message from the user
+    
     user_msg = state["messages"][-1].content
     
-    # Invoke the chain
+    
     response = chain.invoke({"issue": user_msg})
     
-    # Return structure matches your image
+    
     return {"messages": state["messages"] + [response]}
 
-# LOGIC: ROUTER DECISION
-# ---------------------------------------------------------
+
 def router_decision(state):
-    # Get the classification from the Interface Node (the last message added)
-    classification = state["messages"][-1].content.lower().strip()
     
-    # --- Logic Check ---
-    # We check for keywords to ensure robustness even if the LLM adds punctuation
+    classification = state["messages"][-1].content.lower().strip()
     
     if "billing" in classification:
         return "billing_node"
@@ -62,5 +57,4 @@ def router_decision(state):
         return "feedback_node"
         
     else:
-        # Default fallback to feedback/general if it gets confused
         return "feedback_node"
